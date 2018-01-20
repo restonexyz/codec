9209a697600cc8538764a6168affe4332d218010
AndrewLucianLu <972977361@qq.com>
2018-01-20 20:23:02 +0800

    - -上传 c语言的项目

---
+++ c/codeblocks/codeblocks/cspace/cproject-20180120/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+// C语言代码
+// C语言代码
+int main()
+{
+    // 伤害
+    double attach=56.88; //攻击力
+    double attchADD = 4.5; //增加伤害
+    int attachRange=172;// 攻击距离
+
+
+    printf("Hello world!\n");
+    printf("名称：德玛西亚之力·盖伦\n");
+    printf("伤害：%.2lf(%+.1lf)\t攻击距离：%d\n",ac);
+    printf("护甲：27.536(+3.0)\t魔    抗：32.1(1.25)\n");
+    printf("生命：416.28(+84.25)\t生命回复：7.84(0.5)\n");
+    printf("法力：0.0(+0.0)\t法力回复：0.0(0.0)\n");
+    printf("移速：340(%+1lf)\t定    位：上单 打野 辅助\n");
+    printf("点券：450(%+1lf)\t金    币：1000\n");
+ //   qqq();
+    return 1;
+}
