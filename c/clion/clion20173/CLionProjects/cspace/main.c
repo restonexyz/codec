62840308b607c04be8df1386fadcb71cc217dfb9
AndrewLucianLu <972977361@qq.com>
2018-01-21 22:59:46 +0800

    - - 更新C语言练习代码

---
+++ c/clion/clion20173/CLionProjects/cspace/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+// C语言代码
+int main()
+{
+    // 伤害
+    double attach=56.88; //攻击力
+    double attchAdd = 4.5; //增加伤害
+    int attachRange=172;// 攻击距离
+    char  ch='A';
+    char  ch2 = '65';
+    printf("11111:%c\n", ch);
+    printf("11111:%c\n", ch2);
+
+
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("名称：德玛西亚之力·盖伦\n");
+    printf("伤害：%.2lf(%+.1lf)\t攻击距离：%d\n",attach,attchAdd,attachRange);
+    printf("护甲：27.536(+3.0)\t魔    抗：32.1(1.25)\n");
+    printf("生命：416.28(+84.25)\t生命回复：7.84(0.5)\n");
+    printf("法力：0.0(+0.0)\t法力回复：0.0(0.0)\n");
+    printf("移速：340\t\t定    位：上单 打野 辅助\n");
+    printf("点券：450\t\t金    币：1000\n");
+ //   qqq();
+    return 0;
+}
