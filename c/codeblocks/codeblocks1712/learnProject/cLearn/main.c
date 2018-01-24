ff58af00b9a5da5d70fc0a495ac1c85e6c7bc156
AndrewLucianLu <972977361@qq.com>
2018-01-25 01:01:44 +0800

    - - 上传代码

---
+++ c/codeblocks/codeblocks1712/learnProject/cLearn/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// standared 标准
+// input output 输入/输出
+// header 头 .h头文件
+
+int main()  // 返回int，如果是void表示没有返回
+{
+    printf("Hello world!\n");   // 以某种格式来打印
+    printf("Hello world2!\n");
+    return 0;
+}
